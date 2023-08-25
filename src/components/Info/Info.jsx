d5171f944e88900b57d43aa61b0f817df08b6ba7
Armenkn1998 <armen.kn1998@mail.ru>
2023-08-25 11:22:25 +0400

    08.25.2023

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import "./Info.scss"
 import { useSelector } from 'react-redux'
 
-
 export function Info () {
   const {Contact} = useSelector((state)=>state.Contact);
+  console.log(Contact);
   return (
     <div className='Info'>
       <div className='info_container'>
@@ -17,7 +17,7 @@ export function Info () {
                 <p>{el?.text}</p>
               </a>)
               :
-              el?.text === "+37441407148"?
+              (index === 1)?
 
                 (<a href={"tel:"+el?.text} >
                 <img src={el?.logo} alt={el?.title} />
@@ -32,50 +32,7 @@ export function Info () {
                  <p>{el?.text}</p> 
                  </>)
 
-            }
-            
-           {/* <p>{ el?.link ? <a >{el?.text}</a>: */}
-          {/* //   <a > */}
-          {/* //     {el?.text} */}
-          {/* // </a> */}
-          {/* //  }</p> */}
-          {/* //   <p > {el?.title}</p> */}
-            {/* {el?.title === "Հեռախոսահամար" ? (
-              <a href="tel:+37441407148">
-                <img src={el?.logo} alt={el?.title} />
-              </a>
-            ) : el?.title === "Գտնվելու վայր" ? (
-              <a
-                href="https://www.google.com/maps/place/Engined+Inc/@40.1816102,44.5732223,18z/data=!4m9!1m2!2m1!1zINSxLiDVhNWr1a_VuNW11aHVtiAxNSw!3m5!1s0x406abd1625cbce61:0x4df25ed34c798505!8m2!3d40.1819306!4d44.573639!16s%2Fg%2F11spf9w8kf?entry=ttu"
-                target="_black"
-              >
-                <img src={el?.logo} alt={el?.title} />
-              </a>
-            ) : (
-              <img src={el?.logo} alt={el?.title} />
-            )}
-
-
-
-            {
-              el?.title === "Հեռախոսահամար" ? (
-                <a href="tel:+37441407148">
-                  <p > {el?.title}</p>
-                </a>
-              ) : el?.title === "Գտնվելու վայր" ? (
-                <a
-                  href="https://www.google.com/maps/place/Engined+Inc/@40.1816102,44.5732223,18z/data=!4m9!1m2!2m1!1zINSxLiDVhNWr1a_VuNW11aHVtiAxNSw!3m5!1s0x406abd1625cbce61:0x4df25ed34c798505!8m2!3d40.1819306!4d44.573639!16s%2Fg%2F11spf9w8kf?entry=ttu"
-                  target="_black"
-                >
-                  <p className='infop'>{el?.text}</p>
-                </a>
-              ) : <p className='infop'>{el?.text}</p>
-
-
-            } */}
-
-          
-          
+             }     
 
           </div>
         )}
